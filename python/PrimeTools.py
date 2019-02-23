bdcc333574a319532339724db2a08f6135b167a2
Paul Kepley <pakepley@gmail.com>
2019-02-23 11:34:41 -0600

    Added primes below, and fixed logic for whether we must resieve

--- python/PrimeTools.py
+++ python/PrimeTools.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from operator import mul
+from bisect import bisect_right
 
 class PrimeTools:
     '''
@@ -43,7 +44,10 @@ class PrimeTools:
                     self.primeList.append(p)
             else:
                 p = self.n
-    
+
+    def mustExtend(self, m):
+        return len(self.primeList) == 0 or self.n < abs(m)
+                
     def isPrime(self, m):
         m = abs(m)
         if m == 0 or m == 1:
@@ -60,19 +64,20 @@ class PrimeTools:
             return True
         
     def primeDivisors(self, m):
-	primeDivisorList = []
-	nDivisors = 1
+        m = abs(m)
+        primeDivisorList = []
+        nDivisors = 1
 
         # no primes divide 1
         if m == 1:
             return 1
-
-	# needed in case n happens to be prime itself
-        if len(self.primeList) == 0 or self.primeList[-1] < m:
+        
+        # needed in case n happens to be prime itself
+        if self.mustExtend(m):
             self.extend(m+1)
-             
+            
         k = m
-	for p in self.primeList:
+        for p in self.primeList:
             powerOfP = 0
             if k == 1:
                 break
@@ -81,11 +86,41 @@ class PrimeTools:
                 powerOfP += 1
             if powerOfP:
                 primeDivisorList.append((p,powerOfP))
-        
+                
         return(primeDivisorList)
-
+    
     def numDivisors(self, m):
+        m = abs(m)
         primeDivisors = self.primeDivisors(m)	
-	return reduce(mul, [pThTuple[1]+1 for pThTuple in primeDivisors], 1)
-
+        return reduce(mul, [pThTuple[1]+1 for pThTuple in primeDivisors], 1)
         
+    def numPrimesBelow(self, m):
+        m = abs(m)
+        
+        if self.mustExtend(m):
+            self.extend(m+1)
+            
+        if self.primeList[-1] < m:
+            return len(self.primeList)
+        else:
+            return bisect_right(self.primeList, m)
+            
+        
+if __name__ == "__main__":
+    pt = PrimeTools(6)
+    print(pt.primeList)
+    
+    def passes_test(exp, obs):
+        passes_test = exp == obs
+        print("Passes (exp=obs?) {} ?= {} : {}".format(exp, obs, passes_test))
+        return passes_test
+
+    # Test 1
+    n_under_obs = pt.numPrimesBelow(11232134)
+    n_under_exp = 740826    
+    passes_test(n_under_exp, n_under_obs)
+
+    # Test 2
+    biggest_p_exp = 11232083
+    biggest_p_obs = pt.primeList[-1]    
+    passes_test(biggest_p_exp, biggest_p_obs)
