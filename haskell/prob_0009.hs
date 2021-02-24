6fde2ddf6b9f99cadfb9f0a43b7b54a441fb818d
Paul Kepley <pakepley@gmail.com>
2021-02-23 21:07:25 -0600

    Prob 9 solution.

---
+++ haskell/prob_0009.hs
@@ -0,0 +1,14 @@
+{-
+    Problem 9:
+    Find the unique Pythagorean triple satisfying a + b + c = 1000
+
+    Solved: 2021-02-23
+-}
+
+
+solveProb =
+  head  [a * b * c | a <- [1..1000], b <- [1..(1000 - a)],
+                     let c = 1000 - a - b, a*a + b*b == c*c]
+
+main = do
+    putStrLn $ "Solution to Prob 9: " ++ show solveProb
